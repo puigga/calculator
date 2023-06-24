55ab20f9796e1f71abc3444f4bf55d9662a7038b
puiggaritomas <puiggaritomas@gmail.com>
2023-06-24 20:04:44 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+console.log(add(2, 1));
+console.log(subtract(2, 1));
+console.log(multiply(2, 1));
+console.log(divide(2, 2));
